8f82ed7f3255dc0dd2ff6d336b650b08ec6ebf4b
Dan MacDonald <daniel.1.macdonald@gm.com>
2024-08-16 13:32:28 -0400

    add license and clarify comment

--- tools/generate_llvm_coverage.sh
+++ tools/generate_llvm_coverage.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+################################################################################
+# Copyright (c) 2024 Contributors to the Eclipse Foundation
+#
+# See the NOTICE file(s) distributed with this work for additional
+# information regarding copyright ownership.
+#
+# This program and the accompanying materials are made available under the
+# terms of the Apache License Version 2.0 which is available at
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# SPDX-License-Identifier: Apache-2.0
+################################################################################
+
 BLUE='\033[0;34m'
 NC='\033[0m'
 HAS_LLVMCOV=$(cargo install --list | grep cargo-llvm-cov)
@@ -13,7 +26,7 @@ fi
 mkdir -p $COVERAGE_OUT
 
 # generate coverage using llvm
-# RUST_LOG ensures the trace macros are not flagged by the coverage output
+# RUST_LOG ensures the debug macros are not flagged by the coverage output
 RUST_LOG=trace cargo llvm-cov --html --output-dir $COVERAGE_OUT 2>&1 | grep -Ev 'TRACE|DEBUG|WARN'
 
 # generate lcov coverage 
